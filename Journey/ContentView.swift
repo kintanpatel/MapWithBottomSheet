f447aae0b3e77b7eafeaea457861e7388efeffdb
Kintan Patel <kintanpatel4393@gmail.com>
2026-03-25 22:22:18 +0530

    Initial Commit

---
+++ Journey/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Journey
+//
+//  Created by AK on 25/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Journey/JourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  JourneyApp.swift
+//  Journey
+//
+//  Created by AK on 25/03/26.
+//
+
+import SwiftUI
+
+@main
+struct JourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
